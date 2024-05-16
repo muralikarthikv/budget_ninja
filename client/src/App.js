54a5b8bd7c849093c708deb8808c462d46ba3199
murali <murali03kv@gmail.com>
2024-05-16 18:22:47 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
